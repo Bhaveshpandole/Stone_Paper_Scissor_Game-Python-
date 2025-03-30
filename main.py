f6a9472948a05cbd219bd381aa7a4b2af83ced14
Bhavesh Pandole <168208304+Bhaveshpandole@users.noreply.github.com>
2025-03-30 13:24:34 +0530

    Update and rename index.py to main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+#random is a Builtin module
 import random
 
+#random.choice is a method to choose random number any of this list[  ,  ,  ]
 computer = random.choice([1,-1,0])
 
-you = input("Enter your choice: ")
+#you take input from user
+you = input("Please choose only \n Stone(-1) : s \n Paper(1) : p \n Scissor(0) : s \n Enter your choice here: ")
 
+#youDict is a Dictionary to store keypairs
 youDict = {"s":1, "p":-1, "c":0}
 
 younum = youDict[you]
 
+#Statement print to user understanding
+print("You choose: ",you)
 print("The computer choose: ",computer)
 
+#Conditional expression
 if(computer == younum):
     print("Its draw")
 
@@ -33,5 +40,3 @@ elif(computer == 0 and younum == -1):
 
 else:
     print("Something went wrong!")
-
-    print("Please choose only \n Stone : s \n Paper : p \n Scissor : s")
